c1a8c5dfbc155d5844134a5ddf28ea53d652e3a5
Michael Zhao <michaelzhao314@gmail.com>
2022-09-29 18:00:41 -0500

    Updated lexer test to include token names

--- LexerTest.java
+++ LexerTest.java
@@ -4,14 +4,14 @@ import java_cup.runtime.*;
 public class LexerTest{
 
     public static void main(String[] args) {
-        Symbol sym;
+        Symbol s;
         try {
             Lexer lexer = new Lexer(new FileReader(args[0]));
-            for (sym = lexer.next_token(); sym.sym != 0;
-                    sym = lexer.next_token()) {
+            for (s = lexer.next_token(); s.sym != 0;
+                    s = lexer.next_token()) {
 
-                String value = sym.value == null ? "" : " (value=" + sym.value.toString() + ")";
-                System.out.printf("[Line: %-3d Col: %-3d] Token %s%s\n", sym.left, sym.right, sym, value);
+                String value = s.value == null ? "" : " (value=" + s.value.toString() + ")";
+                System.out.printf("[Line: %-3d Col: %-3d] Token %s %s%s\n", s.left, s.right, s, sym.terminalNames[s.sym], value);
             }
         }
         catch (Exception e) {
